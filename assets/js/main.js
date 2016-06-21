757306ff0c5963bf11eb64fcec5d345110d1490a
KiselDima <kisel_work@mail.ru>
2016-06-21 12:52:18 +0300

    go for all

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*global $, jQuery, alert, console*/
+(function () {
+    "use strict";
+    $(document).ready(function () {
+        console.log('main js');
+    });
+}());
+
+
+
+//# sourceMappingURL=main.js.map
